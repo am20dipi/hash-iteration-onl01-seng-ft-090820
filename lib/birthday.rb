8896348192b8223b38dd3d14f9d4a93f416dae0c
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-12 23:15:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,5 +13,10 @@ end
 def age_appropriate_birthday(birthday_kids)
   oldlady = ""
   birthday_kids.each do |kids_name, age|
-    if age >= 12 
+    if age >= 12
+      oldlady = name
+    end
+  end
+  oldlady
+end
     
