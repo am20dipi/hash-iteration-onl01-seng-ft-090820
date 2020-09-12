bb19ab0a5fcb539f4fd27524e43389fcef48079a
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-12 23:05:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,8 +10,7 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+    winner = name
   end
 end
 
